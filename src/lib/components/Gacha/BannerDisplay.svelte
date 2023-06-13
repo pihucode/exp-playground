6ade49ce9281362c6d0a5ad2c20dc1ebf1407bcb
Pinxuan Huang <pinxhuang@gmail.com>
2023-06-13 17:15:58 -0400

    Work on banner animation

--- src/lib/components/Gacha/BannerDisplay.svelte
+++ src/lib/components/Gacha/BannerDisplay.svelte
@@ -1,21 +1,53 @@
 <script>
 	let bannerImage;
 
-	const playAnimation = () => {
-		console.log('playAnimation called!');
-		// reset animation
-		bannerImage.style.animation = 'none';
-		bannerImage.offsetHeight; /* trigger reflow */
+	let animation = 'img__slidein__animation';
+	let isSlideIn = false;
+	let isSlideOut = false;
+
+	const slideIn = () => {
+		reset();
+		setTimeout(() => {
+			isSlideIn = !isSlideIn;
+		}, 10);
+	};
+	const slideOut = () => {
+		reset();
+		setTimeout(() => {
+			isSlideOut = !isSlideOut;
+		}, 10);
+	};
+	const animate = () => {
+		reset();
+
+		isSlideIn = !isSlideIn;
+	};
+	const reset = () => {
+		isSlideIn = false;
+		isSlideOut = false;
 	};
 </script>
 
 <div>
 	<h1>asd</h1>
 	<div class="controls">
-		<button on:click={playAnimation()}>Play animation</button>
+		<button on:click={() => animate()}>Animate</button>
+		<button on:click={() => slideIn()}>Slide In</button>
+		<button on:click={() => slideOut()}>Slide Out</button>
+		<button on:click={() => reset()}>Reset</button>
+
+		<span>Slide In: {isSlideIn}</span>
+		<span>Slide out: {isSlideOut}</span>
+		<span>isHidden: {!isSlideIn || isSlideOut}</span>
 	</div>
 	<div class="banner">
-		<div class="banner__image" bind:this={bannerImage}>
+		<div
+			class="banner__image {animation}"
+			class:img__slidein__animation={isSlideIn}
+			class:img__slideout__animation={isSlideOut}
+			class:hidden={!isSlideIn || !isSlideOut}
+			bind:this={bannerImage}
+		>
 			<img src="/images/Character_Yanqing_Splash_Art.png" alt="banner image" />
 		</div>
 
@@ -31,6 +63,12 @@
 </div>
 
 <style>
+	.controls {
+		margin-bottom: 200px;
+	}
+	.hidden {
+		display: none;
+	}
 	.banner {
 		/* display: flex; */
 		/* flex-direction: row; */
@@ -49,14 +87,18 @@
 		/* background-color: lightseagreen; */
 		width: 600px;
 		height: 450px;
+	}
+	.banner__image img {
+		width: 100%;
+	}
 
+	.img__slidein__animation {
+		background-color: chartreuse;
+		display: block;
 		animation-name: img_slidein;
 		animation-duration: 1s;
 		animation-timing-function: ease-out;
 		animation-fill-mode: forwards;
-	}
-	.banner__image img {
-		width: 100%;
 	}
 	@keyframes img_slidein {
 		from {
@@ -67,6 +109,14 @@
 			opacity: 1;
 			margin-left: 0;
 		}
+	}
+	.img__slideout__animation {
+		background-color: darkred;
+		animation-name: img_slidein;
+		animation-duration: 0.5s;
+		animation-timing-function: ease-in-out;
+		animation-fill-mode: forwards;
+		animation-direction: reverse;
 	}
 
 	.banner__info {
