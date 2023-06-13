c81ee6d992660fb6c8870e746855937a1041589a
Pinxuan Huang <pinxhuang@gmail.com>
2023-06-13 09:37:12 -0400

    Create an animated banner

---
+++ src/lib/components/Gacha/BannerDisplay.svelte
@@ -0,0 +1,106 @@
+<script>
+	let bannerImage;
+
+	const playAnimation = () => {
+		console.log('playAnimation called!');
+		// reset animation
+		bannerImage.style.animation = 'none';
+		bannerImage.offsetHeight; /* trigger reflow */
+	};
+</script>
+
+<div>
+	<h1>asd</h1>
+	<div class="controls">
+		<button on:click={playAnimation()}>Play animation</button>
+	</div>
+	<div class="banner">
+		<div class="banner__image" bind:this={bannerImage}>
+			<img src="/images/Character_Yanqing_Splash_Art.png" alt="banner image" />
+		</div>
+
+		<div class="banner__info">
+			<div class="banner__info__title">
+				<h2>Title</h2>
+			</div>
+			<div class="banner__info__description">
+				<p>Description Lorem ipsum...</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<style>
+	.banner {
+		/* display: flex; */
+		/* flex-direction: row; */
+		position: relative;
+		width: 800px;
+		height: 450px;
+		background-color: lightgray;
+		color: black;
+	}
+
+	.banner__image {
+		position: absolute;
+		top: -150px;
+		z-index: 1;
+
+		/* background-color: lightseagreen; */
+		width: 600px;
+		height: 450px;
+
+		animation-name: img_slidein;
+		animation-duration: 1s;
+		animation-timing-function: ease-out;
+		animation-fill-mode: forwards;
+	}
+	.banner__image img {
+		width: 100%;
+	}
+	@keyframes img_slidein {
+		from {
+			opacity: 0;
+			margin-left: 100px;
+		}
+		to {
+			opacity: 1;
+			margin-left: 0;
+		}
+	}
+
+	.banner__info {
+		position: absolute;
+		bottom: 0px;
+		right: 0px;
+		z-index: 5;
+
+		background-color: cadetblue;
+		width: 300px;
+		padding: 8px 24px;
+		margin: 32px;
+
+		animation-name: info_slidein;
+		animation-duration: 1s;
+		animation-timing-function: ease-out;
+		animation-fill-mode: forwards;
+	}
+
+	@keyframes info_slidein {
+		from {
+			opacity: 0;
+			margin-right: 100px;
+		}
+
+		to {
+			opacity: 1;
+			margin-right: 32px;
+		}
+	}
+
+	.banner__info__title {
+	}
+
+	.banner__info__description {
+	}
+</style>
---
+++ src/routes/gacha/banner/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import BannerDisplay from '$lib/components/Gacha/BannerDisplay.svelte';
+</script>
+
+<div class="page">
+	<BannerDisplay />
+</div>
+
+<style>
+	.page {
+		display: flex;
+		justify-content: center;
+		width: 100vw;
+		height: 100vh;
+		background-color: rgb(74, 74, 74);
+	}
+</style>
