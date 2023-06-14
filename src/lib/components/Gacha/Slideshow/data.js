6e9e34815c7db4522a6c689df46b62850d59a7ef
Pinxuan Huang <pinxhuang@gmail.com>
2023-06-14 09:17:13 -0400

    Add placeholder images for banner

---
+++ src/lib/components/Gacha/Slideshow/data.js
@@ -0,0 +1,22 @@
+// import { writable } from 'svelte/store';
+
+export const images = [
+    {
+        id: 0,
+        name: "Banner 1",
+        imgurl: "/images/Character_Yanqing_Splash_Art.png",
+        attribution: "banner 1",
+    },
+    {
+        id: 1,
+        name: "Banner 2",
+        imgurl: "/images/Jing-Yuan-Splash-Art.png",
+        attribution: "banner 2",
+    },
+    {
+        id: 2,
+        name: "Banner 3",
+        imgurl: "/images/Silver-Wolf-Splash-Art.png",
+        attribution: "banner 3",
+    }
+];
