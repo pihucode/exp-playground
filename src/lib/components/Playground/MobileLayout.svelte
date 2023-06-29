4a65515308617cadeb63dcb5d16241c97fa57859
Pinxuan Huang <pinxhuang@gmail.com>
2023-06-29 13:54:14 -0400

    Make page zoom responsive to window size

---
+++ src/lib/components/Playground/MobileLayout.svelte
@@ -0,0 +1,51 @@
+<script></script>
+
+<div class="container">
+	<div class="box__main">main content</div>
+	<div class="box1">box 1 content</div>
+	<div class="box2">box 2 content</div>
+	<div class="box3">box 3 content</div>
+</div>
+
+<style>
+	.container {
+		position: relative;
+		width: 100%;
+		height: 100vh;
+		overflow: hidden;
+	}
+	.box__main {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		width: 45%;
+		height: 40%;
+		transform: translate(-50%, -50%);
+		background-color: rgb(138, 216, 241);
+	}
+
+	.box1 {
+		position: absolute;
+		top: 5%;
+		left: 4%;
+		width: 10%;
+		height: 15%;
+		background-color: rgb(245, 123, 123);
+	}
+	.box2 {
+		position: absolute;
+		left: 8%;
+		bottom: 12%;
+		width: 20%;
+		height: 20%;
+		background-color: rgb(172, 248, 79);
+	}
+	.box3 {
+		position: absolute;
+		top: 45%;
+		right: 10%;
+		width: 12%;
+		height: 14%;
+		background-color: rgb(145, 123, 245);
+	}
+</style>
---
+++ src/routes/playground/layout/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import MobileLayout from '$lib/components/Playground/MobileLayout.svelte';
+</script>
+
+<div class="page">
+	<MobileLayout />
+</div>
+
+<style>
+	.page {
+		width: 100vw;
+		height: 100vh;
+		background-color: #021027;
+	}
+</style>
