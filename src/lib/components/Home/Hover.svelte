49a6b361c2564e5188d06c85869156c9541b9720
Pinxuan Huang <pinxhuang@gmail.com>
2023-06-30 14:48:41 -0400

    Work on styling in Hover component

--- src/lib/components/Home/Hover.svelte
+++ src/lib/components/Home/Hover.svelte
@@ -4,8 +4,10 @@
 	let entity;
 	let shadow;
 
-	let textAbout =
-		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.';
+	let mainTitle = 'Nekolotl';
+	let subTitle = 'An adoptables gacha';
+	let about =
+		'Official website for Nekolotls. A closed species adoptables gacha game currently in the works.';
 
 	// on mousemove, shift the shadow and entity to create a parallax effect
 	// the shadow is shifted more than the entity to create a depth effect
@@ -14,26 +16,27 @@
 		const x = e.clientX;
 		const y = e.clientY;
 		const entityShift = 20;
-		const shadowShift = 10;
+		const shadowShift = 15;
 
 		entity.style.transform = `translate(${x / entityShift}px, ${y / entityShift}px)`;
-		shadow.style.transform = `translate(${x / shadowShift}px, ${y / shadowShift}px)`;
+		entity.style.filter = `drop-shadow(${50 + x / shadowShift}px ${
+			30 + y / shadowShift
+		}px 10px rgba(0, 0, 0, 0.4))`;
+		// shadow.style.transform = `translate(${x / shadowShift}px, ${y / shadowShift}px)`;
 	};
 </script>
 
 <div class="container" on:mousemove={onMouseMove}>
 	<div class="entity" bind:this={entity}>
-		<img src="/images/Character_Yanqing_Splash_Art.png" alt="image" />
-
-		<!-- <img src="https://i.pinimg.com/564x/24/83/58/248358df207fec2ce4e85b9d924cba94.jpg" /> -->
+		<img style="transform: scaleX(-1);" src="/images/relic-seeker.png" alt="featured nekolotl" />
 	</div>
-	<div class="shadow" bind:this={shadow} />
+	<!-- <div class="shadow" bind:this={shadow} /> -->
 
 	<div class="box__left">
-		<h1 class="title">Main title here</h1>
-		<p class="subtitle">Subtitle here lorem ipsum</p>
-		<p class="about">{textAbout}</p>
-		<div class="button">
+		<h1 class="title">{mainTitle}</h1>
+		<p class="subtitle">{subTitle}</p>
+		<p class="about">{about}</p>
+		<div class="button__container">
 			<button class="btn">button 1</button>
 			<button class="btn">button 2</button>
 		</div>
@@ -52,16 +55,19 @@
 		background-color: transparent;
 		background-image: url('https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/6e13bb66109353.5b0b7773dbca9.jpg');
 		background-size: cover;
+		overflow: hidden;
 	}
 
 	.entity {
 		position: absolute;
-		top: -100px;
-		right: 50px;
+		bottom: 100px;
+		left: 50vw;
 		z-index: 20;
 	}
 	.entity img {
 		width: 800px;
+		max-width: 1000px;
+		filter: drop-shadow(0 0 5px rgba(0, 0, 0, 0.25));
 	}
 
 	.shadow {
@@ -77,7 +83,7 @@
 
 	.entity,
 	.shadow {
-		transition: transform 0.25s linear;
+		/* transition: transform 0.25s linear; */
 	}
 
 	.box__centered {
@@ -92,21 +98,48 @@
 	}
 	.box__left {
 		position: absolute;
-		left: 100px;
+		/* left: 100px; */
+		left: 10em;
 		bottom: 32px;
 		z-index: 10;
+		/* width: 40vw; */
+		width: 600px;
 		max-width: 800px;
 		max-height: 600px;
 		/* background-color: rgba(0, 0, 0, 0.15); */
 		color: white;
-		background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.05));
+		background-image: linear-gradient(
+			to bottom,
+			rgba(0, 0, 0, 0.25),
+			rgba(0, 0, 0, 0.05),
+			rgba(0, 0, 0, 0)
+		);
+		border-radius: 6px;
 		font-family: 'Open Sans', sans-serif;
+		padding: 32px 64px;
+		padding-bottom: 200px;
 	}
 
 	.title {
-		font-size: 150px;
+		font-size: 4em;
 		font-weight: bold;
 		text-transform: uppercase;
 		margin: 32px 0;
+	}
+	.btn {
+		background-color: transparent;
+		border: 1px solid white;
+		color: white;
+		padding: 8px 16px;
+		margin: 0 8px;
+		font-size: 16px;
+		font-weight: bold;
+		cursor: pointer;
+		transition: all 0.25s linear;
+		border-radius: 2px;
+	}
+	.btn:hover {
+		background-color: white;
+		color: gray;
 	}
 </style>
