ff8dcde48ce7cae74d9ba0a135e7424f5c8581bd
Pinxuan Huang <pinxhuang@gmail.com>
2023-07-05 14:51:18 -0400

    Dynamically adjust REM

--- src/lib/components/Playground/MobileLayout.svelte
+++ src/lib/components/Playground/MobileLayout.svelte
@@ -13,40 +13,40 @@
 		width: 100%;
 		height: 100%;
 		overflow: hidden;
-		font-size: 1.2rem;
+		font-size: 1rem;
 	}
 	.box__main {
 		position: absolute;
 		top: 50%;
 		left: 50%;
-		width: 45%;
-		height: 40%;
+		width: 25rem;
+		height: 16rem;
 		transform: translate(-50%, -50%);
 		background-color: rgb(138, 216, 241);
 	}
 
 	.box1 {
 		position: absolute;
-		top: 5%;
-		left: 4%;
-		width: 10%;
-		height: 15%;
+		top: 1.2rem;
+		left: 1.6rem;
+		width: 5rem;
+		height: 5rem;
 		background-color: rgb(245, 123, 123);
 	}
 	.box2 {
 		position: absolute;
-		left: 5rem;
-		bottom: 4.2rem;
+		left: 3rem;
+		bottom: 3.2rem;
 		width: 11.5rem;
 		height: 6.5rem;
 		background-color: rgb(172, 248, 79);
 	}
 	.box3 {
 		position: absolute;
-		top: 45%;
-		right: 10%;
-		width: 12%;
-		height: 14%;
+		top: 13rem;
+		right: 2rem;
+		width: 10rem;
+		height: 6rem;
 		background-color: rgb(145, 123, 245);
 	}
 </style>
