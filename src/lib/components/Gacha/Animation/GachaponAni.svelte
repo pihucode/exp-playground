c3b7f1e2854c4b48bc64140a90b3dd96c2c9ef83
Pinxuan Huang <pinxhuang@gmail.com>
2023-07-06 12:34:23 -0400

    Simple fadeout animation for gachapon

---
+++ src/lib/components/Gacha/Animation/GachaponAni.svelte
@@ -0,0 +1,58 @@
+<script>
+	let isAnimate = true;
+
+	const animate = () => {
+		isAnimate = false;
+		setTimeout(() => {
+			isAnimate = true;
+		}, 1000 * 0.5);
+	};
+</script>
+
+<div class="container">
+	<div class="controls">
+		<button on:click={() => animate()}>Play Animation</button>
+
+		<span>isAnimate: {isAnimate}</span>
+	</div>
+
+	<div class="gachapon">
+		<img src="/images/Character_Yanqing_Splash_Art.png" alt="banner" />
+		<img
+			class:img__animation={isAnimate}
+			src="/images/Character_Yanqing_Splash_Art.png"
+			alt="overlay"
+		/>
+	</div>
+</div>
+
+<style>
+	.container {
+		padding: 32px;
+		position: relative;
+		width: 500px;
+	}
+
+	.gachapon img {
+		position: absolute;
+		top: 100px;
+		left: 0;
+		width: 500px;
+		pointer-events: none;
+	}
+
+	.img__animation {
+		animation: fadeout 0.3s ease-in forwards;
+	}
+
+	@keyframes fadeout {
+		0% {
+			scale: 1;
+			opacity: 1;
+		}
+		100% {
+			scale: 1.4;
+			opacity: 0;
+		}
+	}
+</style>
---
+++ src/routes/gacha/animation/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	import GachaponAni from '$lib/components/Gacha/Animation/GachaponAni.svelte';
+</script>
+
+<div class="page">
+	<GachaponAni />
+</div>
+
+<style>
+	.page {
+		display: flex;
+		justify-content: center;
+		/* padding: 64px; */
+		/* align-items: center; */
+		width: 100vw;
+		height: 100vh;
+		background-color: rgb(74, 74, 74);
+	}
+</style>
